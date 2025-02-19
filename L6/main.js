f873d1d64984d5b68f9868873f3c55be4881f1db
TriggeerCat <zspwdmnplay@gmail.com>
2025-02-19 02:10:35 +0200

    feat: added sixth home task

---
+++ L6/main.js
@@ -0,0 +1,221 @@
+﻿// Task 1
+
+console.log('hello world'.length, 'lorem ipsum'.length, 'javascript is cool'.length);
+
+// Task 2
+
+console.log('hello world'.toUpperCase(), 'lorem ipsum'.toUpperCase(), 'javascript is cool'.toUpperCase());
+
+// Task 3
+
+console.log('HELLO WORLD'.toLowerCase(), 'LOREM IPSUM'.toLowerCase(), 'JAVASCRIPT IS COOL'.toLowerCase());
+
+// Task 4
+
+function trueJoin(arr) {
+    let str = "";
+    for (const arrElement of arr) {
+        if (arrElement !== '') str += (arrElement + ' ');
+    }
+    return str;
+}
+
+console.log(trueJoin(' dirty string   '.split(' ')))
+
+// Task 5
+
+let stringToArray = (str) => str.split(' ');
+console.log(stringToArray('Ревуть воли як ясла повні'));
+
+// Task 6
+
+console.log([10, 8, -7, 55, 987, -1011, 0, 1050, 0].map((value) => value.toString()));
+
+// Task 7
+
+function sortNums(array, direction) {
+    switch (direction) {
+        case 'descending':
+            return array.sort((a, b) => a < b ? 1 : -1);
+        case 'ascending':
+            return array.sort((a, b) => a > b ? 1 : -1);
+        default:
+            return null;
+    }
+}
+
+console.log(sortNums([11, 21, 3], 'descending'));
+console.log(sortNums([11, 21, 3], 'ascending'));
+
+// Task 8
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.filter(course => course.monthDuration > 5));
+console.log(coursesAndDurationArray.map((course, index) => {
+    return {id: index + 1, title: course.title, monthDuration: course.monthDuration};
+}));
+console.log(coursesAndDurationArray.sort((a, b) => a.monthDuration < b.monthDuration ? 1 : -1));
+
+// Task 9
+
+const deck = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'},
+
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+
+    {cardSuit: 'heart', value: '6', color: 'red'},
+    {cardSuit: 'heart', value: '7', color: 'red'},
+    {cardSuit: 'heart', value: '8', color: 'red'},
+    {cardSuit: 'heart', value: '9', color: 'red'},
+    {cardSuit: 'heart', value: '10', color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'}
+];
+
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі дзвіни
+// - всі хрести від 9 та більше
+
+console.log(deck.find(card => card.cardSuit === 'spade' && card.value === 'ace'));
+console.log(deck.filter(card => card.value === '6'));
+console.log(deck.filter(card => card.color === 'red'));
+console.log(deck.filter(card => card.cardSuit === 'diamond'));
+console.log(deck.filter(card => card.cardSuit === 'clubs' && ['9', '10', 'jack', 'queen', 'king', 'ace'].includes(card.value)));
+
+// Task 10
+
+console.log(deck.reduce((accumulator, card) => {
+    switch (card.cardSuit) {
+        case 'spade':
+            accumulator.spades.push(card);
+            break;
+        case 'diamond':
+            accumulator.diamonds.push(card);
+            break;
+        case 'heart':
+            accumulator.hearts.push(card);
+            break;
+        case 'clubs':
+            accumulator.clubs.push(card);
+            break;
+    }
+    return accumulator;
+}, {
+    spades: [],
+    diamonds: [],
+    hearts: [],
+    clubs: []
+}));
+
+// Task 11
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+console.log(coursesArray.filter((course) => course.modules.includes('sass')));
+console.log(coursesArray.filter((course) => course.modules.includes('docker')));
